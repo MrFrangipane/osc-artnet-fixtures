3048c3653c99b50afe6594f7dc5561b5ddcb6a40
MrFrangipane <contact@frangipane.me>
2024-02-11 18:08:33 +0100

    imp(Tristan200): Mood to Color Wheel

--- oscartnetfixtures/components/api.py
+++ oscartnetfixtures/components/api.py
@@ -1,4 +1,5 @@
 import importlib
+import logging
 from typing import Type
 
 from oscartnetdaemon.core.fixture.base import BaseFixture
@@ -7,11 +8,14 @@ from oscartnetfixtures.components.fixtures import octostrip_bar
 from oscartnetfixtures.components.fixtures import tristan_200
 from oscartnetfixtures.components.fixtures import two_bright_par
 
+_logger = logging.getLogger(__name__)
+
 
 class OSCArtnetFixturesAPI:
 
     @staticmethod
     def reload_definitions():
+        _logger.info("Reloading fixtures definitions")
         importlib.reload(octostrip_bar)
         importlib.reload(tristan_200)
         importlib.reload(two_bright_par)
--- oscartnetfixtures/components/fixtures/tristan_200.py
+++ oscartnetfixtures/components/fixtures/tristan_200.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from dataclasses import dataclass
 
@@ -36,33 +37,38 @@ class Tristan200(BaseFixture):
         super().__init__(address)
         self.elapsed = 0
 
+        self._mapping = Tristan200.Mapping()
+        self._previous_color = 0
+        self._previous_gobo1 = 0
+        self._wheels_blackout_timestamp = 0
+
     def map_to_channels(self, mood: Mood, group_position: float) -> list[int]:
         self.elapsed += mood.animation / 10.0
         sym = (group_position * 2.0) - 1.0
 
-        mapping = Tristan200.Mapping()
-        mapping.color = map_to_int(mood.palette, 66, 80)
+        self._mapping = Tristan200.Mapping()
+        self._mapping.color = self._color_wheel(mood)
 
         if mood.texture > .66:
-            mapping.focus = 255
-            mapping.gobo2 = 11
+            self._mapping.focus = 255
+            self._mapping.gobo2 = 11
             dim_factor = 1.0
 
         elif mood.texture > .33:
-            mapping.focus = 255
-            mapping.gobo1 = 28
-            mapping.prism = 26
-            mapping.frost = 34
-            mapping.prism_rotation = 179 + int(group_position * 27)
+            self._mapping.focus = 255
+            self._mapping.gobo1 = 28
+            self._mapping.prism = 26
+            self._mapping.frost = 34
+            self._mapping.prism_rotation = 179 + int(group_position * 27)
             dim_factor = 0.6
 
         else:
-            mapping.prism = 26
-            mapping.frost = 144
+            self._mapping.prism = 26
+            self._mapping.frost = 144
             dim_factor = 0.4
 
         if mood.blinking > .7:
-            mapping.shutter = 121
+            self._mapping.shutter = 121
 
         elif mood.blinking < .3:
             dim_factor *= p_cos(mood.beat_counter * 6.28)
@@ -71,8 +77,60 @@ class Tristan200(BaseFixture):
         if .6 > mood.animation > .3:
             pan = 0.18  # roughly 45, centered
 
-        mapping.pan = map_to_int(pan)
-        mapping.tilt = 40
-        mapping.dimmer = map_to_int(mood.master_dimmer * mood.recallable_dimmer * dim_factor * 0.5)
+        self._mapping.pan = map_to_int(pan)
+        self._mapping.tilt = 40
+        self._mapping.dimmer = map_to_int(mood.master_dimmer * mood.recallable_dimmer * dim_factor * 0.5)
 
-        return list(vars(mapping).values())
+        self._poll_for_wheels_blackout()
+        return list(vars(self._mapping).values())
+
+    def _color_wheel(self, mood:Mood) -> int:
+        if mood.blinking > 0.75:
+            return 64  # open
+
+        mapping = [
+            [0.0, .03, 66],  # red
+            [.03, .05, 67],  # red/orange
+            [.05, .07, 68],  # orange
+            [.07, .10, 69],  # orange/yellow
+            [.10, .14, 70],  # yellow
+            [.14, .22, 71],  # yellow/green
+            [.22, .37, 72],  # green
+            [.37, .55, 78],  # blue
+            [.55, .71, 74],  # cyan
+            # [.71, .75, 76],  # magenta
+            [.71, .98, 80],  # pink
+            # 73
+            # 75
+            # 77
+            # 79
+            [.98, 1.0, 66]   # red
+        ]
+
+        for min_, max_, value in mapping:
+            if min_ <= mood.palette <= max_:
+                distance = abs(self._previous_color - value)
+                if distance > 0:
+                    self._previous_color = value
+                    if distance > 1:
+                        self._start_wheels_blackout()
+
+                return value
+
+        return 128  # fast wheel rotation to debug
+
+    def _start_wheels_blackout(self):
+        """
+        Blacks out for a short period of time while wheels are turning
+        Call whenever you change a parameter associated with a wheel
+        """
+        self._wheels_blackout_timestamp = time.time()
+
+    def _poll_for_wheels_blackout(self):
+        """
+        Blacks out for a short period of time while wheels are turning
+        Call right before returning mapped channels
+        """
+        if time.time() - self._wheels_blackout_timestamp < 0.2:
+            self._mapping.dimmer = 0
+
