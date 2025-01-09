3232b0ed68729f84f9c5962aceeb2095e311d1ef
MrFrangipane <contact@frangipane.me>
2025-01-09 01:12:16 +0100

    fix(HeroWash): color computation is now OK, dimmer and strobe are set to 0 by default

--- oscartnetfixtures/components/fixtures/hero_wash.py
+++ oscartnetfixtures/components/fixtures/hero_wash.py
@@ -13,8 +13,6 @@ _logger = logging.getLogger(__name__)
 
 class HeroWash(BaseFixture):
 
-    MAX_RGBW = 128
-
     @dataclass
     class Mapping:
         pan: int = 0
@@ -23,8 +21,8 @@ class HeroWash(BaseFixture):
         tilt_fine: int = 0
         moving_speed: int = 0
         zoom: int = 0
-        dimmer: int = 255
-        strobe: int = 255
+        dimmer: int = 0
+        strobe: int = 0
         red_1: int = 0
         green_1: int = 0
         blue_1: int = 0
@@ -87,17 +85,17 @@ class HeroWash(BaseFixture):
 
         r, g, b, w = hsl_to_rgbw(hue, 1.0, self._lightness)
 
-        self._mapping.red_1 = int(r * self.MAX_RGBW)
-        self._mapping.green_1 = int(g * self.MAX_RGBW)
-        self._mapping.blue_1 = int(b * self.MAX_RGBW)
-        self._mapping.white_1 = int(w * self.MAX_RGBW)
+        self._mapping.red_1 = int(r * self._mapping.red_1)
+        self._mapping.green_1 = int(g * self._mapping.green_1)
+        self._mapping.blue_1 = int(b * self._mapping.blue_1)
+        self._mapping.white_1 = int(w * self._mapping.white_1)
 
-        self._mapping.red_2 = int(r * self.MAX_RGBW)
-        self._mapping.green_2 = int(g * self.MAX_RGBW)
-        self._mapping.blue_2 = int(b * self.MAX_RGBW)
-        self._mapping.white_2 = int(w * self.MAX_RGBW)
+        self._mapping.red_2 = int(r * self._mapping.red_2)
+        self._mapping.green_2 = int(g * self._mapping.green_2)
+        self._mapping.blue_2 = int(b * self._mapping.blue_2)
+        self._mapping.white_2 = int(w * self._mapping.white_2)
 
-        self._mapping.red_3 = int(r * self.MAX_RGBW)
-        self._mapping.green_3 = int(g * self.MAX_RGBW)
-        self._mapping.blue_3 = int(b * self.MAX_RGBW)
-        self._mapping.white_3 = int(w * self.MAX_RGBW)
+        self._mapping.red_3 = int(r * self._mapping.red_3)
+        self._mapping.green_3 = int(g * self._mapping.green_3)
+        self._mapping.blue_3 = int(b * self._mapping.blue_3)
+        self._mapping.white_3 = int(w * self._mapping.white_3)
